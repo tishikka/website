98e9ad7bb9576266679b688f61eea2bc4abcfff8
tishikka <evgeniya8682@gmail.com>
2024-06-17 18:15:40 +0700

    lessons

--- script.js
+++ script.js
@@ -5,17 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-// script.js
-document.getElementById('flipButton').addEventListener('click', function() {
-  document.getElementById('card').classList.toggle('flipped');
-});
-
-document.getElementById('answerForm').addEventListener('submit', function(event) {
-  event.preventDefault();
-  // Обработка отправленных данных формы
-  const answer1 = document.getElementById('answer1').value;
-  const answer2 = document.getElementById('answer2').value;
-  const answer3 = document.getElementById('answer3').value;
-  alert(`Answers submitted:\n1. ${answer1}\n2. ${answer2}\n3. ${answer3}`);
-  // Можно добавить логику для отправки данных на сервер или другой обработки
-});
+function flipCard() {
+  const card = document.getElementById('card');
+  card.classList.toggle('flipped');
+}
